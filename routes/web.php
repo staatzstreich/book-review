8aba25fa9d2932ae766606d595bbf43779f4407a
Michael Staatz <michael@staatzstreich.de>
2024-09-15 14:00:08 +0200

    Added ratelimiting

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::resource('books', BookController::class)->only([
 Route::resource('books.reviews', ReviewController::class)
     ->scoped(['review' => 'book'])
     ->only(['create', 'store']);
+
+Route::post('books/{book}/reviews', [ReviewController::class, 'store'])
+    ->middleware('throttle:reviews')
+    ->name('books.reviews.store');
