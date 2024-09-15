4ff59fa1eab22285899837a21f2d9a038a14b4e2
Michael Staatz <michael@staatzstreich.de>
2024-09-15 06:27:51 +0200

    How to invalidate the cache

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -54,9 +54,12 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        return view('books.show', ['book' => $book->load(
+        $cacheKey = 'book:'. $book->id;
+        $book = cache()->remember($cacheKey, 3600, fn() => $book->load(
             ['reviews' => fn($query) => $query->latest()],
-        )]);
+        ));
+
+        return view('books.show', ['book' => $book]);
     }
 
     /**
